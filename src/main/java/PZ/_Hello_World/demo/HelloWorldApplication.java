b30197faa1110144f75eb3c4d171cae589b1e763
Gergram <mr.zaluposhka@gmail.com>
2021-07-10 13:28:56 +0300

    README

---
+++ src/main/java/PZ/_Hello_World/demo/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package PZ._Hello_World.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PZ/_Hello_World/demo/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package PZ._Hello_World.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
